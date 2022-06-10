d60757b1446e031b4fbb0bf5aac15a5f834e97c6
LAPTOP-1EG0V295\cctie <cctiely97@gmail.com>
2022-06-10 16:10:30 -0300

    Implementação da camada Api.Service, se comunicando com a camada Api.Data e Api.Domain, criação da Interface UserService em Api.Domain e a implementação da mesma em Api.Service

---
+++ Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll();// vai retornar uma lista de usuários
+
+        Task<UserEntity> Post(UserEntity user);
+
+        Task<UserEntity> Put (UserEntity user);// quem vai trabalhar cm esses metodo será a aplicação
+
+        Task<bool> Delete(Guid id);
+    }
+}
